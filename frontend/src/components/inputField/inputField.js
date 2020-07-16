c53d77e7f0f96f4832b2c57fa01ae002dea56733
Dan Thompson <daniel.24.thompson@bt.com>
2020-07-16 17:03:46 +0100

    Created Form Button and Input Field components, used these to create the login page, still needs form adding a few tweaks to styling

--- frontend/src/components/inputField/inputField.js
+++ frontend/src/components/inputField/inputField.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './inputField.css';
 
-function InputField() {
+function InputField(props) {
+    let iconClass = props.icon + " input-icon";
     return(
-        <input className="inputField"></input>
+        <div className="input-container">
+            <i className= {iconClass}></i>
+            <input type={props.type} className="inputField" placeholder={props.placeholder}/>
+        </div>  
     )
 }
 
--- frontend/src/views/Login/Login.js
+++ frontend/src/views/Login/Login.js
@@ -1,12 +1,19 @@
 import React from "react";
 import Title from '../../components/title/title';
 import InputField from "../../components/inputField/inputField";
+import Button from '../../components/formButton/formButton';
 
 function Login() {
   return (
     <div className="login">
       <Title title={"Login"}/>
-      <InputField />
+      <form>
+        <>
+        <InputField icon={"fas fa-envelope"} type="email" placeholder="Enter Your Email"/>
+        <InputField icon={"fas fa-lock"} type="password" placeholder="Enter Your Password"/>
+        <Button content="Submit"></Button>
+        </>
+      </form>
     </div>
   );
 }
