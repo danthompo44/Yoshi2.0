1e9905edc520ece606ee341eafd635c1d29b1adc
Dan Thompson <daniel.24.thompson@bt.com>
2020-07-17 13:03:01 +0100

    Created Sign Up Page as per design, also created the form wrapper component

--- frontend/src/views/SignUp/SignUp.js
+++ frontend/src/views/SignUp/SignUp.js
@@ -1,7 +1,10 @@
 import React from 'react';
+
 import Title from '../../components/title/title';
 import InputField from '../../components/inputField/inputField';
 import Button from '../../components/squareButton/squareButton';
+import FormWrapper from '../../components/form-wrapper/form-wrapper';
+import InputLabel from '../../components/inputField/inputLabel';
 
 import './SignUp.css';
 
@@ -9,12 +12,17 @@ function SignUp() {
     return (
         <div className="sign-up-container">
             <Title title={"Sign Up"}/>
-            <form id="sign-up-form" method="POST">
-                <InputField type="email" icon={"fas fa-envelope"} placeholder="Email Address"/>
-                <InputField type="password" icon={"fas fa-lock"} placeholder="Password"/>
-                <InputField type="password" icon={"fas fa-lock"} placeholder="Confirm Password"/>
+            <FormWrapper>
+            <form class="form-inner-wrapper" method="POST">
+                <InputLabel for="email" content="Email Address:"/>
+                <InputField type="email" icon={"fas fa-envelope"} placeholder="Email Address" name="email"/>
+                <InputLabel for="password" content="Password"/>
+                <InputField type="password" icon={"fas fa-lock"} placeholder="Password" name="password"/>
+                <InputLabel for="confirm-password" content="Confirm Password"/>
+                <InputField type="password" icon={"fas fa-lock"} placeholder="Confirm Password" name="confirm-password"/>
                 <Button content="Register"/>
             </form>
+            </FormWrapper>      
         </div>
     );
 }
