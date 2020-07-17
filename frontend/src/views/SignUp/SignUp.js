c162336251a345b985910841885fcc676a8f9a5c
Dan Thompson <daniel.24.thompson@bt.com>
2020-07-17 09:40:42 +0100

    Sign Up Page is now complete, still an existent bug with the moving icons

--- frontend/src/views/SignUp/SignUp.js
+++ frontend/src/views/SignUp/SignUp.js
@@ -1,12 +1,22 @@
 import React from "react";
 import Title from '../../components/title/title';
+import InputField from '../../components/inputField/inputField';
+import Button from '../../components/squareButton/squareButton';
+
+import './SignUp.css';
 
 function SignUp() {
-  return (
-    <div className="sign-up">
-      <Title title={"Sign Up"}/>
-    </div>
-  );
+    return (
+        <div className="sign-up-container">
+            <Title title={"Sign Up"}/>
+            <form id="sign-up-form" method="POST">
+                <InputField type="email" icon={"fas fa-envelope"} placeholder="Email Address"/>
+                <InputField type="password" icon={"fas fa-lock"} placeholder="Password"/>
+                <InputField type="password" icon={"fas fa-lock"} placeholder="Confirm Password"/>
+                <Button content="Register"/>
+            </form>
+        </div>
+    );
 }
 
 export default SignUp;
