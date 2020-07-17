bc3efdb040571b295ee104213eabf1470dd57638
Dan Thompson <daniel.24.thompson@bt.com>
2020-07-17 11:49:19 +0100

    Finished creating the Input Field Component

--- frontend/src/views/Login/Login.js
+++ frontend/src/views/Login/Login.js
@@ -1,28 +1,37 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+
 import Title from '../../components/title/title';
 import InputField from "../../components/inputField/inputField";
 import SquareButton from '../../components/squareButton/squareButton';
+
 import './Login.css';
-import { Link } from "react-router-dom";
+import '../../App.css';
+
 
 function Login() {
   return (
     <div id="login-container">
-      <div id="sign-up-button-container">
-        <Link to="/auth/sign-up">
-          <SquareButton content="Sign Up"/>
-        </Link>
-      </div>
       <Title title={"Login"}/>
-      <form id="login-form" method="GET">
-        <>
-        <InputField icon={"fas fa-envelope"} type="email" placeholder="Enter Your Email"/>
-        <InputField icon={"fas fa-lock"} type="password" placeholder="Enter Your Password"/>
-        <SquareButton content="Submit"/>
-        </>
-      </form>
+      <FormWrapper>
+        <form class="form-inner-wrapper" method="GET">
+          <>
+          <InputField icon={"fas fa-envelope"} type="email" placeholder="Enter Your Email"/>
+          <InputField icon={"fas fa-lock"} type="password" placeholder="Enter Your Password"/>
+          <SquareButton content="Submit"/>
+          </>
+        </form>
+        </FormWrapper>
     </div>
   );
+}
+
+function FormWrapper({children}) {
+  return (
+    <div className="form-wrapper">
+      {children}
+    </div>
+  )
 }
 
 export default Login;
