a7f66d5f552128e531f6efc8aa8a6fa1465c70d5
Dan Thompson <daniel.24.thompson@bt.com>
2020-07-31 10:56:27 +0100

    Created all methods for the console api

--- server/src/controllers/consoleController.js
+++ server/src/controllers/consoleController.js
@@ -1,8 +1,15 @@
-const { createErrorData } = require('../helpers');
+const { request, response } = require('express');
+
+const { createErrorData, isDataNullOrUndefined, throwNotFoundError } = require('../helpers');
 const db = require('../models');
 
 const Console = db.Console;
 
+/**
+ * A method to retrieve all consoles
+ * @param {request} req Express Request Object
+ * @param {response} res Express Response Object
+ */
 async function getAll(req, res) {
     try {
         const consoles = await Console.findAll();
@@ -13,4 +20,24 @@ async function getAll(req, res) {
     }
 }
 
-module.exports = { getAll };
+/**
+ * A method to retrieve a console by its id
+ * @param {request} req Express Request Object
+ * @param {response} res Express Response Object
+ * @param {number} req.params.id A console id
+ */
+async function getById(req, res){
+    try{
+        const console = await Console.findByPk(req.params.id);
+        if(isDataNullOrUndefined(console)){
+            throwNotFoundError();
+        }
+        return res.status(200).json(console);
+    }
+    catch(err){
+        const error = createErrorData(err);
+        return res.status(error.code).json(error.error);
+    }
+}
+
+module.exports = { getAll, getById };
--- server/src/routes/consoleRouter.js
+++ server/src/routes/consoleRouter.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const controller = require('../controllers/consoleController');
 
 router.get('/', controller.getAll);
+router.get('/console/:id', controller.getById);
 
 module.exports = router;
